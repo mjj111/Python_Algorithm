5f092d69d6ef2b66eaa30f6796188195b819e0b8
mjj111 <skatks1016@naver.com>
2022-06-06 18:47:25 +0900

    코드 변경사항 커밋

---
+++ 부캠CodingTest/준비운동2/16916.py
@@ -0,0 +1,33 @@
+P, S = input().strip(), input().strip()
+
+
+def get_table(x):
+    arr = [0 for _ in range(len(x))]
+    j = 0
+    for i in range(1, len(x)):
+        while j > 0 and x[i] != x[j]:
+            j = arr[j-1]
+        if x[i] == x[j]:
+            j += 1
+            arr[i] = j
+    return arr
+
+
+def kmp(word, find):
+    result = 0
+    j = 0
+    for i in range(len(word)):
+        while j > 0 and word[i] != find[j]:
+            j = table[j-1]
+        if word[i] == find[j]:
+            if j == len(find)-1:
+                result += 1
+                j = table[j]
+            else:
+                j += 1
+    return result
+
+
+table = get_table(S)
+result = kmp(P, S)
+print(1 if result else 0)
