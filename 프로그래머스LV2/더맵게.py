1aa5243adfc68e567f5e25d2c5b0827bf1a63a90
mjj111 <skatks1016@naver.com>
2022-07-01 19:34:55 +0900

    import 문 옮기기

--- 프로그래머스LV2/더맵게.py
+++ 프로그래머스LV2/더맵게.py
@@ -1,9 +1,10 @@
+import heapq
 def solution(scoville, K):
-    import heapq
+
     answer = 0
-    heapq.heapify(scoville) #list to heapq
+    heapq.heapify(scoville) 
     
-    while scoville[0] < K: #가장 작은 수가 기준보다 낮다면 계속
+    while scoville[0] < K: 
         if len(scoville)>1:
             answer += 1
             first = heapq.heappop(scoville) 
