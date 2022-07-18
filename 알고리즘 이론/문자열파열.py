00d7a301ff6bc3da3dd7ce7a7ab8980af85dc6b6
mjj111 <skatks1016@naver.com>
2022-07-18 22:12:36 +0900

    문자얄 공부

---
+++ 알고리즘 이론/문자열파열.py
@@ -0,0 +1,16 @@
+s = input().strip()
+p = input().strip()
+n = len(s)
+m = len(p)
+lastChar = p[-1]
+
+stack = []
+for char in s:
+    stack.append(char)
+    if char == lastChar and "".join(stack[-m:]) == p:
+        del stack[-m:]
+
+if len(stack) == 0:
+    print("FRULA")
+else:
+    print("".join(stack))
