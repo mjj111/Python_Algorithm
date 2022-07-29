73b2691d5fee2c3b82c1fc50d3094011604bb49a
mjj111 <skatks1016@naver.com>
2022-07-30 05:37:04 +0900

    구현

---
+++ 짝지어제거하기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    if len(s) % 2 == 1: return 0 
+    if len(s) == 2: 
+        return 1 if s[0] == s[1] else 0
+            
+    stack = [s[0]]
+    
+    for v in s[1:]:
+        if stack and stack[-1] == v:
+            stack.pop()
+        else:
+            stack.append(v)
+            
+    return 0 if stack else 1
