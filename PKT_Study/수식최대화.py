adcfdd95534dbb5b1f836fbd142c9cefd88dbbe3
mjj111 <skatks1016@naver.com>
2022-08-03 23:21:11 +0900

    카카오블라인드 코테문제

--- PKT_Study/수식최대화.py
+++ PKT_Study/수식최대화.py
@@ -16,21 +16,16 @@ def splitExpression(expression, sep):
     return numbers, operations
  
 def solution(expression):
-    # 연산자 종류
-    sep = ['*', '+', '-']
     
-    # 주어진 expression을 숫자와 연산자로 구분해서 저장할 리스트
+    sep = ['*', '+', '-']
     numbers, operations = splitExpression(expression, sep)
  
     answer = 0
-    # 가능한 모든 조합에 대해 우승 상금을 정해본다.
     for priority_ops in list(P(sep, 3)):
         temp_num = numbers[:]
         temp_op =  operations[:]
  
-        # 연산자 우선순위의 순열을 각각 조회
         for op in priority_ops:
-            # 우선순위를 갖는 연산자부터 전부 연산을 수행해본 후 결과값 저장
             while True:
                 try:
                     opIndex = temp_op.index(op)
@@ -47,7 +42,6 @@ def solution(expression):
                 except:
                     break
  
-        # 최종 계산 결과가 최대값을 갖는지 확인
         result = abs(temp_num[0])
         if result > answer:
             answer = result
