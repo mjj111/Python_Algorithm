0028a3f0b25073c7d75a294da44a9bc41e536f46
mjj111 <skatks1016@naver.com>
2022-09-25 14:35:57 +0900

    TCP 파이썬 소캣 프로그래밍

---
+++ bridge.py
@@ -0,0 +1,51 @@
+import socket
+
+HOST = '127.0.0.1'
+now_PORT = 9999
+
+print("시작 ")
+
+bridge_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+bridge_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+bridge_socket.bind((HOST, now_PORT))
+
+bridge_socket.listen(3)
+
+client_socket, addr = bridge_socket.accept()
+
+print('Connected by', addr)
+
+result = ""
+
+equation_f = client_socket.recv(1024)
+equation = equation_f.decode() 
+
+num1_f = client_socket.recv(1024)
+num2_f = client_socket.recv(1024)
+to_PORT = 0
+data = 0
+
+
+if equation == "+" or equation == "-":
+    to_PORT = 9992
+    bridge_socket.connect((HOST, to_PORT))
+    #값 송신 
+    bridge_socket.sendall(equation_f)
+    bridge_socket.sendall(num1_f)
+    bridge_socket.sendall(num2_f) 
+    
+    
+else:
+    to_PORT = 9991
+    bridge_socket.connect((HOST, to_PORT))
+    #값 송신 
+    bridge_socket.sendall(equation_f)
+    bridge_socket.sendall(num1_f)
+    bridge_socket.sendall(num2_f) 
+    
+data = bridge_socket.recv(1024)
+bridge_socket.sendall(data) 
+bridge_socket.close()
+client_socket.close()
--- client.py
+++ client.py
@@ -1,28 +1,27 @@
 import socket
-# 접속하고 싶은 서버의 주소를 입력한다.
-# ip주소를 직접 입력할 수 도 있고 hostname도 입력 가능하다.
-#host = '127.0.0.1'
-host = "192.168.45.216"
-# 접속하고 싶은 포트를 입력한다.
-port = 8080
 
-#IPv4 체계, TCP 타입 소켓 객체를 생성
+print("hi")
+HOST = '127.0.0.1'  
+message = input('방정식을 입력하여 주세요 ex(1+1,2-1) : ')
+message = list(message)
+equation = message[1]
+num1 = message[0]
+num2 = message[2]
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 9999
+client_socket.connect((HOST, PORT))
 
-# 지정한 host와 prot를 통해 서버에 접속합니다.
-client_socket.connect((host, port))
+#값 송신 
+client_socket.sendall(equation.encode())
+client_socket.sendall(num1.encode())
+client_socket.sendall(num2.encode()) 
 
-while 1:
-    print("아무 것도 입력하지 않으면 종료 됩니다.")
-    string = input("서버에 보내고 싶은 데이터를 입력하세요.(ex 1+1, 5-2...) : ")
 
-    # 메시지 전송
-    client_socket.sendall(string.encode())
-    if string == "": break
-    # 메시지 수신
-    receive_data = client_socket.recv(1024)
-    print("결과값은 \"", receive_data.decode(), "\" 입니다.", sep="")
 
-# 소켓을 닫는다.
+# 메시지를 수신
+data = client_socket.recv(1024)
+print('원하신 방정식의 결과값 :', repr(data.decode()))
+
+# 소켓을 닫습니다.
 client_socket.close()
-print("접속을 종료합니다.")
